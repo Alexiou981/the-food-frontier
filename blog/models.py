131dda39c92b2a78ea3eea3688dc5397ae02034b
Evangelos Alexiou <evansa44@gmail.com>
2024-06-05 12:41:12 +0000

    feat: add Post Model to database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+POST_STATUS = ((0, "Draft"), (1, "Published"))
+APPROVAL_STATUS = ((0, "Not-Approved"),(1, "Approved"))
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=POST_STATUS, default=0)
+    approval_status = models.IntegerField(choices=APPROVAL_STATUS, default=0)
+    modified_on = models.DateTimeField(auto_now=True)
