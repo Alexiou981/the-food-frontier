6f9d58a551cde81eaa12661ab3dfdd313cb07520
Evangelos Alexiou <evansa44@gmail.com>
2024-06-09 14:45:26 +0000

    feat: add PostAdmin and PostCommentAdmin classes

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Post, PostComment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(PostComment)
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    summernote_fields = ('content',)
+
+
+@admin.register(PostComment)
+class PostCommentAdmin(SummernoteModelAdmin):
+
+    list_display = ('post', 'body')
+    search_fields = ['body']
+    list_filter = ('approval_status',)
+    summernote_fields = ('body',)
+
