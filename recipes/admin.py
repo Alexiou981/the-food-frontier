7e2f3ebbec3d8c7df0a741459f225dde54ab7729
Evangelos Alexiou <evansa44@gmail.com>
2024-06-09 18:09:01 +0000

    feat: creare summernote classes for Recipe and RecipeComment models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import Recipe, RecipeComment
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 
-admin.site.register(Recipe)
-admin.site.register(RecipeComment)
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'author', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    summernote_fields = ('instructions', 'ingredients',)
+
+@admin.register(RecipeComment)
+class RecipeCommentAdmin(SummernoteModelAdmin):
+
+    list_display = ('body', 'approval_status')
+    search_fields = ['body']
+    list_filter = ('approval_status',)
+    summernote_fields = ('body',)
