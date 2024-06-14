466a53533f2c61cc249e20d34feb20aa1d589853
Evangelos Alexiou <evansa44@gmail.com>
2024-06-14 21:16:35 +0000

    chore: import include and rewrite path for recipes url

--- the_food_frontier/urls.py
+++ the_food_frontier/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from about.views import my_about_me
-from recipes.views import my_recipes
+
 
 
 
@@ -26,6 +26,6 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"),
     path('about/', my_about_me, name="about"),
-    path('recipes/', my_recipes, name="recipes")
+    path("recipes/", include("recipes.urls"), name="recipes")
 
 ]
