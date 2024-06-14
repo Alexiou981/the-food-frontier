784ecc3086e71003e7a8e62d1af9ae630dbad524
Evangelos Alexiou <evansa44@gmail.com>
2024-06-14 21:18:02 +0000

    chore: add RecipeList class and import Recipe model

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from .models import Recipe
 
 # Create your views here.
 
-def my_recipes(request):
-    return HttpResponse("Hello, Recipes!")
+class RecipeList(generic.ListView):
+    queryset = Recipe.objects.all()
+    template_name = 'recipes/recipes.html'
