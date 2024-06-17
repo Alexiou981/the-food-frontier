7ffef80eea59c3e85c5df78523edfa7dbe68e3c8
Evangelos Alexiou <evansa44@gmail.com>
2024-06-17 15:53:40 +0000

    add views, urls and templates for recipes_detail u

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('recipes/', views.RecipeList.as_view(), name='recipes'),
+    path('<slug:slug>/', views.recipes_detail, name='recipes_detail'),
 ]
