1e429161104200aa81cb36e06a293a5acb0b9d32
Evangelos Alexiou <evansa44@gmail.com>
2024-06-27 17:40:53 +0000

    feat: add link to recipes page for recipe form

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='recipes'),
     path('<slug:slug>/', views.recipes_detail, name='recipes_detail'),
-    path('recipe-form', views.users_recipe, name='users_recipe')
+    path('recipe-form', views.users_recipe, name='recipe_form')
 ]
