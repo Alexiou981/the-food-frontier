4e255cff1434848911a10dd59dbb613bee6450e8
Evangelos Alexiou <evansa44@gmail.com>
2024-07-03 15:51:03 +0000

    feat: add edit functionality to recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='recipes'),
     path('<slug:slug>/', views.recipes_detail, name='recipes_detail'),
-    path('recipe-form', views.users_recipe, name='recipe_form')
+    path('recipe-form', views.users_recipe, name='recipe_form'),
+    path('edit_recipe/<slug:slug>/', views.recipe_edit, name='recipe_edit')
 ]
