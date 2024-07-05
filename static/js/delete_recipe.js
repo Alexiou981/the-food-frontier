fed40baf829a9ff46a5728a5aae8d2c54825cffc
Evangelos Alexiou <evansa44@gmail.com>
2024-07-05 10:32:22 +0000

    feat: add delete functionality to users recipes for logged in user

---
+++ static/js/delete_recipe.js
@@ -0,0 +1,10 @@
+/* Gets detele modal and delete button */
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteButtons = document.getElementsByClassName("btn-delete");
+
+/* Modal appears when delete button is clicked */
+for (let button of deleteButtons) {
+    button.addEventListener("click", (e) => {
+        deleteModal.show();
+    });
+}
