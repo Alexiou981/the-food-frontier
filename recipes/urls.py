c975ee7b166e61e3be241a28b6abe26a6343bff0
Evangelos Alexiou <evansa44@gmail.com>
2024-07-05 10:32:22 +0000

    feat: add delete functionality to users recipes for logged in user

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.RecipeList.as_view(), name='recipes'),
     path('<slug:slug>/', views.recipes_detail, name='recipes_detail'),
     path('recipe-form', views.users_recipe, name='recipe_form'),
-    path('edit_recipe/<slug:slug>/', views.recipe_edit, name='recipe_edit')
+    path('edit_recipe/<slug:slug>/', views.recipe_edit, name='recipe_edit'),
+    path('delete_recipe/<slug:slug>/', views.recipe_delete, name='delete_recipe')
 ]
