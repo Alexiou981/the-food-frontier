66e1fadfd2d5999e65874dc6d2405737a46e6d98
Evangelos Alexiou <evansa44@gmail.com>
2024-07-06 11:50:54 +0000

    chore: add ref to JS function

--- static/js/delete_recipe.js
+++ static/js/delete_recipe.js
@@ -1,8 +1,9 @@
-/* Gets detele modal and delete button */
+// Code taken from Codestar walkthrough by Code Institute
+// Gets detele modal and delete button 
 const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 const deleteButtons = document.getElementsByClassName("btn-delete");
 
-/* Modal appears when delete button is clicked */
+// Modal appears when delete button is clicked 
 for (let button of deleteButtons) {
     button.addEventListener("click", (e) => {
         deleteModal.show();
