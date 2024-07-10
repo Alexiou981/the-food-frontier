246bcec9ace8b92224f5b87a1fd318154cf1cacd
Evangelos Alexiou <evansa44@gmail.com>
2024-07-10 12:49:28 +0000

    feat: create views, url and template for my recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,12 @@
 from . import views
 from django.urls import path
+from .views import MyRecipesList
 
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='recipes'),
     path('<slug:slug>/', views.recipes_detail, name='recipes_detail'),
     path('recipe-form', views.users_recipe, name='recipe_form'),
     path('edit_recipe/<slug:slug>/', views.recipe_edit, name='recipe_edit'),
-    path('delete_recipe/<slug:slug>/', views.recipe_delete, name='delete_recipe')
+    path('delete_recipe/<slug:slug>/', views.recipe_delete, name='delete_recipe'),
+    path('my_recipes', MyRecipesList.as_view(), name='my_recipes'),
 ]
