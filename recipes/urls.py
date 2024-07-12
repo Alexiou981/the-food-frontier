61a2b238d8986e097c8427ffa22a9c1aa7d86ece
Evangelos Alexiou <evansa44@gmail.com>
2024-07-12 10:28:53 +0000

    fix: rearrange urlpatterns to fix error of my recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,11 +4,11 @@ from .views import MyRecipesList, CuisineView
 
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='recipes'),
+    path('my_recipes/', MyRecipesList.as_view(), name='my_recipes'),
     path('<slug:slug>/', views.recipes_detail, name='recipes_detail'),
     path('recipe-form', views.users_recipe, name='recipe_form'),
     path('edit_recipe/<slug:slug>/', views.recipe_edit, name='recipe_edit'),
     path('delete_recipe/<slug:slug>/', views.recipe_delete, name='delete_recipe'),
-    path('my_recipes/', MyRecipesList.as_view(), name='my_recipes'),
     path('cuisine/<str:cuisine>/', CuisineView, name='cuisine_filter')
     
 ]
