ebf203669c3ce86cf8380ef1e391837c3440ed91
Evangelos Alexiou <evansa44@gmail.com>
2024-07-23 14:12:29 +0000

    chore: modify file to pass Python linter

--- recipes/admin.py
+++ recipes/admin.py
@@ -3,20 +3,21 @@ from .models import Recipe
 from django_summernote.admin import SummernoteModelAdmin
 from .forms import RecipeForm
 
-# Register your models here.
 
+# Register your models here.
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
     list_display = ('title', 'author', 'cuisine', 'status')
     search_fields = ['title']
     list_filter = ('status',)
 
-
-# Code taken from here:
-# https://djangopatterns.readthedocs.io/en/latest/models/automatically_filling_user.html
     def save_model(self, request, obj, form, change):
+        """
+        Function ensures that the author is assigned as logged in user
+        for new recipes.
+        Code taken from here:
+        https://djangopatterns.readthedocs.io/en/latest/models/automatically_filling_user.html
+        """
         if not obj.author.id:
             obj.author = request.user
-        obj.save() 
-    
-
+        obj.save()
