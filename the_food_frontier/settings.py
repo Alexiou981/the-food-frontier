4c5387a13ffa8961012abf6ffc57bf49354e2bda
Evangelos Alexiou <evansa44@gmail.com>
2024-07-23 17:20:11 +0000

    fix: add SSL/TLS configuration

--- the_food_frontier/settings.py
+++ the_food_frontier/settings.py
@@ -176,3 +176,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
