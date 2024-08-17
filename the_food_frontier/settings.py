cbe73e557031e5f7ea45b90a999e584c5e744e93
Evangelos Alexiou <evansa44@gmail.com>
2024-08-17 23:58:44 +0000

    fix: adjust settings to resolve error loading register page

--- the_food_frontier/settings.py
+++ the_food_frontier/settings.py
@@ -127,20 +127,16 @@ REMOVED_SECRET_KEY = os.environ.get("REMOVED_SECRET_KEY")
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-        UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-        MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-        CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-        NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
